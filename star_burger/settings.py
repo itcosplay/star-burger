62f5210c10ff89011484f82114e71ec4bfefb73f
Konstantine Pialov <konstantinepialov@Konstantines-MacBook-Air.local>
2022-05-24 20:31:27 +0300

    Updated db settings

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -87,8 +87,14 @@ WSGI_APPLICATION = 'star_burger.wsgi.application'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# DATABASES = {
+#     'default': dj_database_url.config(conn_max_age=None)
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=None)
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
