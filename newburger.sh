f4e61286107cfcd5184bf24d3b198a73701a5a46
Konstantine Pialov <konstantinepialov@Konstantines-MacBook-Air.local>
2022-05-28 16:13:23 +0300

    Updated newburger

--- newburger.sh
+++ newburger.sh
@@ -2,5 +2,10 @@
 set -e
 
 cd /opt/star-burger
+docker compose -f docker-compose.prod.yml down
+git fetch
+git pull
 docker compose -f docker-compose.prod.yml build
 docker compose -f docker-compose.prod.yml up -d
+
+echo star-burged was updated and it will work soon!
